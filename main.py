32bdae44efeb8439775d8c1a8e425d20b35f9a0f
Aleksandr <30879949+Santistox@users.noreply.github.com>
2021-05-09 00:42:37 +0300

    new map generator

--- main.py
+++ main.py
@@ -7,22 +7,34 @@ class Battle:
         self.debug = debug # debug mode to print log on screen
         self.width = width  # width of map
         self.height = height # height of map
-        self.game_map = newMap(width, height)
+        self.game_map = [['.']*width]*height
+        
+        if debug:
+            for row in self.game_map:
+                for elem in row:
+                    print("%%-%ds" % 2 % elem, end="")
+                print()
+            print("Map generated successfully!")
 
-    def printMap(self, ppi):
-        for y in range(self.height):
-            for x in range(self.width):
-                print("%%-%ds" % ppi % self.game_map[x][y], end="")
-            print()
 
+    # def printMap(self, ppi):
+    #     for y in range(self.height):
+    #         for x in range(self.width):
+    #             print("%%-%ds" % ppi % self.game_map[x][y], end="")
+    #         print()
+
+
+def newMap(width, height):
+    gmap = [[0]*width]*height
+    print(gmap)
 
 def main():
     os.system('clear')
     battle = Battle(30, 30, True)
 
-    g = MapGrid(30, 30)
-    g.walls = get_walls(g)
-    draw_grid(g)
+    # g = MapGrid(30, 30)
+    # g.walls = get_walls(g)
+    # draw_grid(g)
     print("Map generated successfully!")
 
 
--- ship_generator.py
+++ ship_generator.py
@@ -1,4 +1,5 @@
 
+from random import randint, choice
 
 class ShipGrid:
     def __init__(self):
@@ -11,8 +12,42 @@ class ShipGrid:
         self.ships = [] # location of ships on the map of each class
 
 # print map
+def newMap(width, height, ptc):
+    gmap = [['.']*width]*height
+    bmap = []
+    for i in range(int(height*width*ptc)//2):
+        x = randint(1, width-1)
+        y = randint(1, height-2)
+        gmap[x][y] = '#'
+        gmap[x + choice([-1, 0, 1])][y + choice([-1, 0, 1])] = '#'
+        # bmap.append((x, y))
+        # bmap.append((x + choice([-1, 0, 1]), y + choice([-1, 0, 1])))
+    print(bmap)
+    # for y in range(height):
+    #     for x in range(width):
+    #         if (x, y) in bmap:
+    #             gmap[x][y] = '#'
+    #             print("%%-%ds" % 2 % gmap[x][y], end="")
+    #             # print("%%-%ds" % 2 % '#', end="")
+    #         else:
+    #             gmap[x][y] = '-'
+    #             print("%%-%ds" % 2 % gmap[x][y], end="")
+    #             # print("%%-%ds" % 2 % '.', end="")
+    #     print()
+    # if ptc:
+    #     for x in range(width):
+    #         for y in range(height):
+    #             if (x, y) in bmap:
+    #                 gmap[x][y] = '#'
+                
+    for row in gmap:
+        for elem in row:
+            print("%%-%ds" % 2 % elem, end="")
+        print()     
+        # print(row)
+    print(gmap)
 
-
+# newMap(30,30, 0.1)
 
 
 
